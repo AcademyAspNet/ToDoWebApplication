2768bf2bba3ed44d9a7ebb16717cfa26e9280c1a
Academy <Academy@AcademyTop>
2025-05-12 16:28:12 +0300

    Display tasks at Tasks page

---
+++ Pages/Tasks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoWebApplication.Pages
+{
+    public class TasksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
