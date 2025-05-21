065dbe3882e76ddc801932ec951e7c86a9863223
Academy <Academy@AcademyTop>
2025-05-21 17:29:38 +0300

    Added new task form & fs persistence

--- Pages/Tasks/New.cshtml.cs
+++ Pages/Tasks/New.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ToDoWebApplication.Models;
@@ -18,16 +19,30 @@ namespace ToDoWebApplication.Pages.Tasks
         {
         }
 
+        public string? ErrorMessage { get; private set; }
+
         [BindProperty]
+        [Required(ErrorMessage = "У задачи должен быть заголовок")]
+        [MinLength(3, ErrorMessage = "Заголовок задачи слишком короткий")]
+        [MaxLength(128, ErrorMessage = "Заголовок задачи слишком длинный")]
         public required string Title { get; set; }
 
         [BindProperty]
+        [MaxLength(2048, ErrorMessage = "Описание задачи слишком длинное")]
         public string? Description { get; set; }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                ErrorMessage = "Указаны некорректные данные.";
+                return Page();
+            }
+
             UserTask task = new UserTask() { Title = Title, Description = Description };
             _taskService.AddTask(task);
+
+            return RedirectToPage("/Tasks/Index");
         }
     }
 }
--- Services/Implementations/TaskService.cs
+++ Services/Implementations/TaskService.cs
@@ -1,10 +1,24 @@
-﻿using ToDoWebApplication.Models;
+﻿using System.Text.Json;
+using ToDoWebApplication.Models;
 
 namespace ToDoWebApplication.Services.Implementations
 {
     public class TaskService : ITaskService
     {
-        private readonly List<UserTask> _tasks = new List<UserTask>();
+        private readonly List<UserTask> _tasks;
+
+        public TaskService()
+        {
+            try
+            {
+                string serializedTasks = File.ReadAllText("tasks.json");
+                _tasks = JsonSerializer.Deserialize<List<UserTask>>(serializedTasks);
+            }
+            catch (FileNotFoundException)
+            {
+                _tasks = new List<UserTask>();
+            }
+        }
 
         public List<UserTask> GetTasks()
         {
@@ -14,6 +28,13 @@ namespace ToDoWebApplication.Services.Implementations
         public void AddTask(UserTask task)
         {
             _tasks.Add(task);
+            Save();
+        }
+
+        private void Save()
+        {
+            string serializedTasks = JsonSerializer.Serialize(_tasks, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("tasks.json", serializedTasks);
         }
     }
 }
